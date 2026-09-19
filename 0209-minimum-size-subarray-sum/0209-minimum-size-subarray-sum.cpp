5116f04b305fd14d88f67679133341dc7867c007
Gaurav Yadav <149584598+gauravvns2005@users.noreply.github.com>
2026-09-19 21:44:21 +0530

    Time: 4 ms (8.5%), Space: 41.9 MB (73.87%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
@@ -5,21 +5,25 @@ public:
 
         int left = 0;
         int sum = 0;
-        int res = INT_MAX;
+        int minLen = INT_MAX;
 
-        for(int right = 0; right < n; right++){
+        for(int right = 0; right < n; right++)
+        {
             sum += arr[right];
 
-
-            while(sum >= target){
-                res = min(res, right - left + 1);
+            while(sum >= target)
+            {
+                minLen = min(minLen, right - left +1);
                 sum -= arr[left];
                 left++;
-
-
             }
         }
 
-        return res == INT_MAX ? 0 : res;
-    }
+        if(minLen == INT_MAX)
+        {
+            return 0;
+        }
+        return minLen;
+
+    }      
 };
